a788f7db8f74abe6afaa005dd4672d72a4d1c82c
Jeff Browning <jeff@craftcoded.com>
2018-06-23 16:26:45 -0700

    skeleton navigation and routes for sign-in

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
-  render () {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!!!!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import LandingPage from './Landing';
+import SignUpPage from './SignUp';
+import SignInPage from './SignIn';
+import PasswordForgetPage from './PasswordForget';
+import HomePage from './Home';
+import AccountPage from './Account';
+
+import * as routes from '../constants/routes';
+
+import Navigation from './Navigation';
+
+const App = () =>
+  <Router>
+    <div>
+      <Navigation />
+
+      <hr/>
+
+      <Route
+        exact path={routes.LANDING}
+        component={() => <LandingPage />}
+      />
+      <Route
+        exact path={routes.SIGN_UP}
+        component={() => <SignUpPage />}
+      />
+      <Route
+        exact path={routes.SIGN_IN}
+        component={() => <SignInPage />}
+      />
+      <Route
+        exact path={routes.PASSWORD_FORGET}
+        component={() => <PasswordForgetPage />}
+      />
+      <Route
+        exact path={routes.HOME}
+        component={() => <HomePage />}
+      />
+      <Route
+        exact path={routes.ACCOUNT}
+        component={() => <AccountPage />}
+      />
+    </div>
+  </Router>
 
 export default App;
