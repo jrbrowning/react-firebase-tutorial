6167ea3f1d76a83914b530a9de298334d9346985
Jeff Browning <jeff@craftcoded.com>
2018-06-24 22:15:54 -0700

    updates to Naviagation and SignUp logic

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,6 +6,7 @@ import * as routes from '../constants/routes';
 const Navigation = () =>
   <div>
     <ul>
+      <li><Link to={routes.SIGN_UP}>Sign UP</Link></li>
       <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
       <li><Link to={routes.LANDING}>Landing</Link></li>
       <li><Link to={routes.HOME}>Home</Link></li>
